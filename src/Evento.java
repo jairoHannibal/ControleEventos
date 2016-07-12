8d31c6213f7aa0724260f4a4d47f7dceeaa0db90
Jairo Oliveira <oliveirajairo01@gmail.com>
2016-07-12 16:51:30 -0300

    Adicionando projeto Eventos

---
+++ src/Evento.java
@@ -0,0 +1,35 @@
+import java.awt.List;
+
+public class Evento {
+	String descricao;
+	String nome;
+	Boolean ativo;
+	List atividades;
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Boolean getAtivo() {
+		return ativo;
+	}
+	public void setAtivo(Boolean ativo) {
+		this.ativo = ativo;
+	}
+	public List getAtividades() {
+		return atividades;
+	}
+	public void setAtividades(List atividades) {
+		this.atividades = atividades;
+	}
+	
+	
+
+}
